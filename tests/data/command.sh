61051ebf574aa8a0f59bbae0bd6f8f2cf6a01a2d
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-04-09 09:45:21 +0000

    update: success run test command

--- tests/data/command.sh
+++ tests/data/command.sh
@@ -2,8 +2,8 @@
 lldb ./target/debug/simpleaf -- quant  \
 		   -c 10xv3 \
 		   -t 8     \
-		   -1 selected_R1_reads.fastq \
-		   -2 selected_R2_reads.fastq     \
+		   -1 tests/data/selected_R1_reads.fastq \
+		   -2 tests/data/selected_R2_reads.fastq     \
 		   -i tests/data/index     \
 		   -u -r cr-like     \
 		   -m tests/data/index/t2g_3col.tsv     \
